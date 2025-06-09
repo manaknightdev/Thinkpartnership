f195a6aa5cd8225cb8ee5dcbdc9d93bb50e43769
[dyad] <git@dyad.sh>
2025-06-09 17:30:48 +0100

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/Header";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } => "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { Handshake, Users, Home, DollarSign, ShieldCheck, Lightbulb, Quote, Info } from "lucide-react";
