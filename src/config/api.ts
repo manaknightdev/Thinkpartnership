1328786335775a6005f0871eabd93b51099c46bb
Idera-oluwa <ideraoluwaisa2003@gmail.com>
2025-07-14 16:12:47 +0100

    changed base url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: 'http://localhost:5172',
+  BASE_URL: 'https://baas.mytechpassport.com',
   ENDPOINTS: {
     // Customer Authentication
     AUTH: {
